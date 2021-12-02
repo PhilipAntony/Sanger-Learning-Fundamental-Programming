c3f8a130832f652291db37a1a49d46dac7385a10
PhilipAntony <philipantony121@gmail.com>
2021-12-02 12:17:58 +0700

    Day-2

---
+++ Day-2/string.js
@@ -0,0 +1,23 @@
+// let nama = "Philip Antony";
+// console.log(nama.length);
+// console.log(nama.indexOf("Antony"));
+
+// let angka = [1, 5, 7, 8, 4, 9];
+// console.log(angka.indexOf(9));
+// console.log(angka.length);
+
+// let nama = "Philip Antony";
+// console.log(nama.substr(nama.indexOf("o"), 3));
+
+// let nama = prompt("Masukkan nama anda : ");
+// let _3hurufAkhir = nama.substr(nama.length - 3, 3);
+
+// let nama = "Philip Antony";
+// let nama = prompt("Masukkan nama anda : ");
+// let _3hurufAkhir = nama.split(" ");
+// // alert("Maka, 3 huruf terakhir pada nama anda adalah: " + _3hurufAkhir);
+// console.log(_3hurufAkhir);
+
+let nama = prompt("Masukkan nama anda : ");
+let perbaikiNama = nama.toUpperCase();
+alert("Nama anda adalah :" + perbaikiNama);
---
+++ Day-2/variable.js
@@ -0,0 +1,33 @@
+// ERROR VARIABLE (SYNTAX JAVASCRIPT)
+
+// let this = "Philip";
+// console.log(this);
+
+// let function = "Philip"
+// console.log(function);
+
+// let for = "Philip";
+// console.log(for);
+
+// let while = "Philip";
+// console.log(while);
+
+// let true = "Philip";
+// console.log(true);
+
+// PENJUMLAHAN STRING DAN NUMBER
+// let angka1 = "20";
+// let angka2 = 22;
+// let jumlah = Number(angka1) + angka2;
+// console.log(jumlah);
+
+// Test Type data
+// let nama = "Philip";
+// let angka = 20;
+// let benarSalah = true;
+// let array12_ = [1, 2, 3];
+
+// console.log(typeof nama);
+// console.log(typeof angka);
+// console.log(typeof benarSalah);
+// console.log(typeof array12_);
