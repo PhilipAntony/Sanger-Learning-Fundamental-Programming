c16a37314dae195620a1d52740f14a0482cd3d7b
PhilipAntony <philipantony121@gmail.com>
2021-12-06 14:22:13 +0700

    Day-3

---
+++ Day-3/replace.js
@@ -0,0 +1,30 @@
+// Replace
+// let nama = "Philip Antony";
+
+// console.log(nama.replace("i", "a"));
+// console.log(nama.replace(/i/g, "a"));
+
+// toString
+// let umur = 18;
+// console.log(umur);
+// console.log(typeof umur);
+
+// console.log(umur.toString());
+// console.log(typeof umur.toString());
+
+// ParseInt / ParseFloat
+// console.log(parseInt("123"));
+// console.log(parseFloat("123"));
+
+// console.log(parseInt("1234.5678"));
+// console.log(parseFloat("1234.5678"));
+
+// console.log(parseInt("Halo Dunia"));
+// console.log(parseFloat("Halo Dunia"));
+
+// Basic Math Object
+console.log(Math.PI);
+console.log(Math.abs(-4.7));
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(64));
+console.log(Math.cbrt(8));
---
+++ Day-3/script.js
@@ -0,0 +1,62 @@
+// Ujian 1
+
+// alert("Harga Buah (Apel = Rp 10.000/kg, Anggur = Rp 15.000/kg, Jeruk = Rp 20.000/kg)");
+
+// const jumlahApel = prompt("Berapa kilogram apel yang ingin anda beli?");
+// const jumlahAnggur = prompt("Berapa kilogram anggur yang ingin anda beli?");
+// const jumlahJeruk = prompt("Berapa kilogram jeruk yang ingin anda beli?");
+
+// const totalHarga = jumlahApel * 10000 + jumlahAnggur * 15000 + jumlahJeruk * 20000;
+
+// alert(`Total belanjaan anda adalah Rp ${totalHarga}`);
+
+// const uangPembeli = prompt("Masukkan jumlah uang yang anda bayar");
+
+// const kembalian = uangPembeli - totalHarga;
+
+// alert(`Terimakasih, uang kembalian anda sebesar : ${kembalian}`);
+
+// Ujian 2
+
+// const platNomor = prompt(`Masukkan plat nomor kendaraan anda`);
+
+// const platNomors = platNomor.split(" ");
+// alert(`Nomor pada plat anda adalah: ${platNomors[1]}`);
+
+// Soal 3
+
+// const isi = "pt kabar medan global";
+
+// const medan = isi.substr(isi.indexOf("medan"), 5).toUpperCase();
+
+// const hasil = isi.split(" ");
+
+// console.log(hasil[0], hasil[1], medan, hasil[3]);
+
+// Soal 4
+// const inputUser = prompt(`Apa yang ingin anda hitung? (kecepatan /jarak/waktu)`);
+// const perbaikiInputUser = inputUser.toLowerCase();
+
+// if (perbaikiInputUser == "kecepatan") {
+//   const jarakTempuh = prompt("Masukkan jarak tempuh: ");
+//   const waktuTempuh = prompt("Masukkan waktu tempuh:");
+
+//   const kecepatan = jarakTempuh / waktuTempuh;
+//   alert(`Maka kecepatannya adalah: ${kecepatan} m/s`);
+// } else if (perbaikiInputUser == "jarak") {
+//   const kecepatan = prompt("Masukkan kecepatan: ");
+//   const waktuTempuh = prompt("Masukkan waktu tempuh: ");
+
+//   const jarakTempuh = kecepatan * waktuTempuh;
+//   alert(`Maka jarak tempuhnya adalah: ${jarakTempuh} m`);
+// } else if (perbaikiInputUser == "waktu") {
+//   const kecepatan = prompt("Masukkan kecepatan:");
+//   const jarakTempuh = prompt("Masukkan jarak tempuh: ");
+
+//   const waktuTempuh = jarakTempuh / kecepatan;
+//   alert(`Maka waktu tempuhnya adalah: ${waktuTempuh} s`);
+// } else {
+//   alert("Input anda tidak ada dalam pilihan");
+// }
+
+// Soal 5
